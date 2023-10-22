f0a41e9e0fc7a2431b68e2711e99521cc93490d7
sheridanchris <christiansheridan@outlook.com>
2023-10-22 07:28:18 -0500

    :alembic:

--- users/christian/home.nix
+++ users/christian/home.nix
@@ -31,6 +31,10 @@
       userName = "sheridanchris";
       userEmail = "christiansheridan@outlook.com";
     };
+    gh = {
+      enable = true;
+      extensions = [ pkgs.gh-dash ];
+    };
     zsh = {
       enable = true;
       enableAutosuggestions = true;
@@ -68,62 +72,6 @@
       enable = true;
       plugins = with pkgs.obs-studio-plugins; [ ];
     };
-    vscode = {
-      enable = true;
-      package = pkgs.vscode.fhs;
-      enableUpdateCheck = false;
-      enableExtensionUpdateCheck = false;
-      extensions = with pkgs.vscode-extensions; [
-        ms-dotnettools.csharp
-        ionide.ionide-fsharp
-        bbenoist.nix
-        eamodio.gitlens
-        vscode-icons-team.vscode-icons
-        mkhl.direnv
-        ms-azuretools.vscode-docker
-        haskell.haskell
-        justusadam.language-haskell
-        catppuccin.catppuccin-vsc
-        editorconfig.editorconfig
-        ocamllabs.ocaml-platform
-        usernamehw.errorlens
-        adpyke.codesnap
-        elixir-lsp.vscode-elixir-ls
-        gruntfuggly.todo-tree
-      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-        {
-          name = "vscode-template-fsharp-highlight";
-          publisher = "alfonsogarciacaro";
-          version = "1.7.0";
-          sha256 = "sha256-yht+l6PcGK1w+xShv6psrQ4WP1pV7B5ALSyTqn9oE6g=";
-        }
-        {
-          name = "vscode-nuget-gallery";
-          publisher = "patcx";
-          version = "0.0.24";
-          sha256 = "sha256-qinjKSc0890V/uNGhd23pcY05WxWRWEGO4yjMIpMj70=";
-        }
-      ];
-      userSettings = {
-        "workbench.iconTheme" = "vscode-icons";
-        "gitlens.codelens.enabled" = true;
-        "editor.formatOnSave" = true;
-        "editor.mouseWheelZoom" = true;
-        "editor.fontSize" = 15;
-        "editor.fontFamily" = "'CommitMono', 'JetBrains Mono', 'Font Awesome 6 Free', 'monospace', monospace";
-        "editor.fontLigatures" = true;
-        "FSharp.inlayHints.enabled" = false;
-        "FSharp.inlayHints.typeAnnotations" = false;
-        "FSharp.inlayHints.parameterNames" = false;
-        "haskell.manageHLS" = "PATH";
-        "[fsharp]" = { "editor.defaultFormatter" = "ionide.ionide-fsharp"; };
-      };
-    };
-    # TODO: Nix LSP ??
-    # TODO: F# ??
-    # TODO: Haskell ??
-    # TODO: Figure out keybinds
-    # TODO: Figure out which plugins need to be configured
     neovim = {
       enable = true;
       defaultEditor = true;
@@ -169,6 +117,7 @@
         nvim-dap
         Ionide-vim
         haskell-tools-nvim
+        vim-nix
 
         # Autocompletion
         luasnip
@@ -177,242 +126,13 @@
         cmp_luasnip
       ];
       extraPackages = with pkgs; [
-        ripgrep # Required to search for selections in files.
-        fsautocomplete # FSAC - Required for Ionide (F#)
-        haskellPackages.haskell-debug-adapter # Required by Haskell-tools
+        ripgrep
+        fsautocomplete
+        haskellPackages.haskell-debug-adapter
+        rnix-lsp
+        lua-language-server
       ];
-      extraLuaConfig = ''
-          vim.g.loaded_netrw = 1
-          vim.g.loaded_netrwPlugin = 1
-          vim.opt.termguicolors = true
-
-          vim.g.mapleader = ' '
-          vim.g.maplocalleader = ' '
-
-          -- formatting
-          vim.api.nvim_create_autocmd('BufWritePre', {
-            callback = function(ev)
-            vim.lsp.buf.format()
-          end,
-          })
-
-          require('neo-tree').setup {
-            window = {
-              mappings = {
-                ['l'] = "open"
-              }
-            }
-          }
-          require('nvim-web-devicons').setup()
-          require('lualine').setup()
-
-
-          vim.cmd([[
-            nnoremap <silent> <F2> <CMD>NeoTreeFloatToggle<CR>
-            tnoremap <silent> <F2> <C-\><C-n><CMD>NeoTreeFloatToggle<CR>
-            nnoremap <silent> <F1> <CMD>FloatermToggle<CR>
-            tnoremap <silent> <F1> <C-\><C-n><CMD>FloatermToggle<CR>
-          ]])
-
-          require('catppuccin').setup({
-            flavour = "mocha"
-          })
-          require('kanagawa').setup({
-            theme = "dragon",
-            background = {
-              dark = "dragon",
-              light = "lotus"
-            },
-          })
-
-          require('zen-mode').setup({
-            window = {
-              width = 90,
-              options = {
-                number = true,
-                relativenumber = true
-              }
-          }
-          })
-
-          require('telescope').setup()
-          pcall(require('telescope').load_extension, 'fzf')
-          do 
-            local tsb = require('telescope.builtin')
-            vim.keymap.set('n', '<leader>?'      , tsb.oldfiles   , { desc = '[?] Find recently opened files' })
-            vim.keymap.set('n', '<leader><space>', tsb.buffers    , { desc = '[ ] Find existing buffers' })
-            vim.keymap.set('n', '<leader>sf'     , tsb.find_files , { desc = '[SF] Search Files' })
-            vim.keymap.set('n', '<leader>sF'     , tsb.git_files  , { desc = '[SF] Search Git Files' })
-            vim.keymap.set('n', '<leader>sh'     , tsb.help_tags  , { desc = '[SH] Search Help' })
-            vim.keymap.set('n', '<leader>sg'     , tsb.live_grep  , { desc = '[SG] Search by Grep' })
-            vim.keymap.set('n', '<leader>sd'     , tsb.diagnostics, { desc = '[SD] Search Diagnostics' })
-            vim.keymap.set('n', '<leader>sr'     , tsb.registers  , { desc = '[SR] Search Registers' })
-
-          -- Configure Treesitter
-          require('nvim-treesitter.configs').setup {
-            highlight = { enable = true },
-            indent = { enable = true },
-            incremental_selection = {
-              enable = true,
-              keymaps = {
-                init_selection = '<c-space>',
-                node_incremental = '<c-space>',
-                scope_incremental = '<c-s>',
-                node_decremental = '<M-space>',
-              },
-            },
-          }
-
-          vim.keymap.set('n', '<leader>/', function()
-            tsb.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-              previewer = false,
-            })
-            end, { desc = '[/] Fuzzily search in current buffer' })
-          end
-
-          vim.keymap.set('n', '<leader>zz', function()
-            require('zen-mode').toggle()
-          end)
-
-          vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
-          vim.keymap.set('n', '<leader><F5>', vim.cmd.UndotreeToggle)
-
-          vim.o.background = 'dark'
-          vim.cmd.colorscheme 'kanagawa'
-
-          vim.opt.nu = true
-          vim.opt.relativenumber = true
-          vim.opt.tabstop = 4
-          vim.opt.softtabstop = 4
-          vim.opt.shiftwidth = 4
-          vim.opt.expandtab = true
-          vim.opt.smartindent = true
-          vim.opt.wrap = false
-          vim.opt.hlsearch = false
-          vim.opt.incsearch = true
-
-          -- Configure Auto Complete
-          local cmp = require('cmp')
-          local luasnip = require('luasnip')
-
-          luasnip.config.setup {}
-
-          cmp.setup {
-            snippet = {
-              expand = function(args)
-              luasnip.lsp_expand(args.body)
-            end,
-            },
-          mapping = cmp.mapping.preset.insert {
-            ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-            ['<C-f>'] = cmp.mapping.scroll_docs(4),
-            ['<C-Space>'] = cmp.mapping.complete {},
-            ['<CR>'] = cmp.mapping.confirm {
-              behavior = cmp.ConfirmBehavior.Replace,
-              select = true,
-            },
-            ['<Tab>'] = cmp.mapping(function(fallback)
-              if cmp.visible() then
-                cmp.select_next_item()
-              elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-              else
-                fallback()
-              end
-            end, { 'i', 's' }),
-            ['<S-Tab>'] = cmp.mapping(function(fallback)
-              if cmp.visible() then
-                cmp.select_prev_item()
-              elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-              else
-                fallback()
-              end
-            end, { 'i', 's' }),
-          },
-          sources = {
-            { name = 'nvim_lsp' },
-            { name = 'luasnip' },
-          },
-        }
-
-        -- Haskell tools configuration
-
-        local ht = require('haskell-tools')
-        local def_opts = { noremap = true, silent = true, }
-        ht.start_or_attach {
-          hls = {
-            on_attach = function(client, bufnr)
-              local opts = vim.tbl_extend('keep', def_opts, { buffer = bufnr, })
-              -- haskell-language-server relies heavily on codeLenses,
-              -- so auto-refresh (see advanced configuration) is enabled by default
-              vim.keymap.set('n', '<space>ca', vim.lsp.codelens.run, opts)
-              vim.keymap.set('n', '<space>hs', ht.hoogle.hoogle_signature, opts)
-              vim.keymap.set('n', '<space>ea', ht.lsp.buf_eval_all, opts)
-            end,
-          },
-        }
-
-        -- Suggested keymaps that do not depend on haskell-language-server:
-        local bufnr = vim.api.nvim_get_current_buf()
-        -- set buffer = bufnr in ftplugin/haskell.lua
-        local opts = { noremap = true, silent = true, buffer = bufnr }
-
-        -- Toggle a GHCi repl for the current package
-        vim.keymap.set('n', '<leader>rr', ht.repl.toggle, opts)
-        -- Toggle a GHCi repl for the current buffer
-        vim.keymap.set('n', '<leader>rf', function()
-          ht.repl.toggle(vim.api.nvim_buf_get_name(0))
-        end, def_opts)
-
-        vim.keymap.set('n', '<leader>rq', ht.repl.quit, opts)
-
-        -- Detect nvim-dap launch configurations
-        -- (requires nvim-dap and haskell-debug-adapter)
-        ht.dap.discover_configurations(bufnr)
-
-        -- LSP Configuration
-        -- Setup language servers.
-        local lspconfig = require('lspconfig')
-
-        -- Global mappings.
-        -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-        vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-        vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-        vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-        vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
-
-        -- Use LspAttach autocommand to only map the following keys
-        -- after the language server attaches to the current buffer
-        vim.api.nvim_create_autocmd('LspAttach', {
-          group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-          callback = function(ev)
-            -- Enable completion triggered by <c-x><c-o>
-            vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-            -- Buffer local mappings.
-            -- See `:help vim.lsp.*` for documentation on any of the below functions
-            local opts = { buffer = ev.buf }
-            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-            vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-            vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-            vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-            vim.keymap.set('n', '<space>wl', function()
-              print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-            end, opts)
-            vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-            vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-            vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-            vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-            vim.keymap.set('n', '<space>f', function()
-              vim.lsp.buf.format { async = true }
-            end, opts)
-        end,
-        })
-      '';
+      extraLuaConfig = lib.fileContents ../../config/neovim/init.lua;
     };
     firefox = {
       enable = true;
@@ -778,6 +498,11 @@
                       tags = [ "vim" "learning" ];
                     }
                     {
+                      name = "Effective Neovim: Instant IDE";
+                      url = "https://www.youtube.com/watch?v=stqUbv-5u2s";
+                      tags = [ "vim" "learning" ];
+                    }
+                    {
                       name = "Neovim Configuration (typecraft)";
                       url = "https://www.youtube.com/playlist?list=PLsz00TDipIffxsNXSkskknolKShdbcALR";
                       tags = [ "vim" "learning" ];
@@ -786,6 +511,26 @@
                       name = "Vim Cheatsheet";
                       url = "https://devhints.io/vim";
                       tags = [ "vim" ];
+                    }
+                    {
+                      name = "Vim Colorschemes";
+                      url = "https://vimcolorschemes.com/";
+                      tags = [ "vim" ];
+                    }
+                  ];
+                }
+                {
+                  name = "Git";
+                  bookmarks = [
+                    {
+                      name = "Git Cheatsheet";
+                      url = "https://education.github.com/git-cheat-sheet-education.pdf";
+                      tags = [ "git" ];
+                    }
+                    {
+                      name = "GitHub CLI";
+                      url = "https://cli.github.com/";
+                      tags = [ "git" "github" ];
                     }
                   ];
                 }
@@ -808,32 +553,6 @@
         };
       };
     };
-    # i3status = {
-    #   enable = true;
-    #   modules = {
-    #     "wireless wlan0" = {
-    #       position = 1;
-    #       settings = {
-    #         format_up = "W: (%quality at %essid, %bitrate) %ip";
-    #         format_down = "W: down";
-    #       };
-    #     };
-    #     "tztime local" = {
-    #       position = 2;
-    #       settings = {
-    #         format = "%Y-%m-%d %H:%M:%S";
-    #       };
-    #     };
-    #     "memory" = {
-    #       position = 3;
-    #       settings = {
-    #         format = "%used";
-    #         threshold_degraded = "10%";
-    #         format_degraded = "MEMORY: %free";
-    #       };
-    #     };
-    #   };
-    # };
   };
 
   home.packages = with pkgs; [
@@ -856,7 +575,7 @@
     libreoffice
     element-desktop
     entr
-    gimp
+    slack
   ];
 
   xsession.windowManager.i3 =
