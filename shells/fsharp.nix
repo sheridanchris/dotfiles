59eb0c3365f0598649544d6acfe53d8792d41361
sheridanchris <christiansheridan@outlook.com>
2024-01-05 17:19:00 -0600

    :construction:

--- shells/fsharp.nix
+++ shells/fsharp.nix
@@ -1,18 +1,31 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
     flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, pre-commit-hooks, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+      let pkgs = import nixpkgs { inherit system; };
       in
       with pkgs;
       {
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              fantomas = {
+                enable = true;
+                name = "fantomas";
+                description = "Format your F# code with fantomas.";
+                entry = "dotnet fantomas";
+                files = "(\\.fs$)|(\\.fsx$)";
+              };
+            };
+          };
+        };
         devShells.default = mkShell {
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
           packages = [
             fsautocomplete
             dotnet-sdk_8
